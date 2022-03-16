09f04b72c0b6d79b60e3d9f1437817e19e2b18cc
Rodrigo Azevedo <Rodrigo>
2022-03-16 16:24:27 -0300

    Variáveis de ambiente

--- main.go
+++ main.go
@@ -4,17 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	config "github.com/rodrigoaz7/api-go-elasticsearch/config"
 	enderecoController "github.com/rodrigoaz7/api-go-elasticsearch/controllers/endereco"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	rotas := mux.NewRouter().StrictSlash(true)
-	rotas.HandleFunc("/", enderecoController.Get).Methods("GET")
-	rotas.HandleFunc("/", enderecoController.Create).Methods("POST")
-	var port = ":8090"
+	config.Init()
+	routes := mux.NewRouter().StrictSlash(true)
+	routes.HandleFunc("/", enderecoController.Get).Methods("GET")
+	routes.HandleFunc("/", enderecoController.Create).Methods("POST")
+	port := os.Getenv("LOCAL_PORT")
 	fmt.Println("Server running in port:", port)
-	log.Fatal(http.ListenAndServe(port, rotas))
+	log.Fatal(http.ListenAndServe(port, routes))
 }
