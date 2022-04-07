312373a0984fc96af50d542a0b8982202bdb2def
Rodrigo Azevedo <Rodrigo>
2022-04-07 14:16:16 -0300

    removing commentaries

--- publisher/controllers/pix/pix_test.go
+++ publisher/controllers/pix/pix_test.go
@@ -16,23 +16,3 @@ func TestGetBody(t *testing.T) {
 		t.Errorf("Expect len > 0 on '%d'", len(body))
 	}
 }
-
-// func TestGenerateTransaction(t *testing.T) {
-// 	request := http.Request{}
-// 	new_body_content := `{
-// 		"target_account": "*******-1",
-// 		"source_account": "TESTFIELD",
-// 		"target_mail": "*******@gmail.com",
-// 		"source_mail": "*******@gmail.com",
-// 		"value": 0.00
-// 	}`
-// 	request.Body = ioutil.NopCloser(strings.NewReader(new_body_content))
-// 	body := getBody(&request)
-// 	data, err := generateTransaction(body)
-// 	if err != nil {
-// 		t.Errorf("Not expect error on '%s'", err)
-// 	}
-// 	if data.SourceAccount != "TESTFIELD" {
-// 		t.Errorf("Source account expected '%s' on '%s'", "TESTFIELD", data.SourceAccount)
-// 	}
-// }
